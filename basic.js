d40bdb1970dfaf2f7365f9837f53d1d6f582eb8d
Agutr0t <abak.93@gmail.com>
2023-03-25 22:43:12 +0100

    feature / close and open contact menu

--- basic.js
+++ basic.js
@@ -1,2 +1,12 @@
 /*jshint esversion: 6 */
 /* jshint node: true */
+
+const openSidebar = () => {
+  document.getElementsByClassName("openContactMenu")[0].style.display = "none";
+  document.getElementsByClassName("contactMenu")[0].style.display = "flex";
+};
+
+const closeSidebar = () => {
+  document.getElementsByClassName("openContactMenu")[0].style.display = "flex";
+  document.getElementsByClassName("contactMenu")[0].style.display = "none";
+};
